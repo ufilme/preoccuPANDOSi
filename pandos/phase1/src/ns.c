f8f469710848c6c77a43b23aac92c1bf48eadc3f
BMMXIV <bernymm2002@gmail.com>
2023-02-19 19:13:49 +0100

    add: ns.c comments

--- pandos/phase1/src/ns.c
+++ pandos/phase1/src/ns.c
@@ -2,6 +2,7 @@
 #include <pandos_types.h>
 #include <ns.h>
 
+//pid type namespace
 static nsd_t pid_nsd[MAXPROC];
 static LIST_HEAD(pid_nsFree_h);
 static LIST_HEAD(pid_nsList_h);
@@ -10,14 +11,14 @@ void initNamespaces(){
     INIT_LIST_HEAD(&pid_nsFree_h);
 
     for (int i = 0; i < MAXPROC; i++){
-        // pid list
+        //adds nsd in pid nsd to list of free pid type nsd
         nsd_t *ns = &pid_nsd[i];
         list_add(&ns->n_link, &pid_nsFree_h);
-        // other lists
     }
 };
 
 nsd_t *getNamespace(pcb_t *p, int type){
+    //return namespace of type "type" of process p
     return p->namespaces[type];
 };
 
@@ -25,6 +26,7 @@ int addNamespace(pcb_t *p, nsd_t *ns){
     pcb_t *pChild;
     struct list_head *cursor;
 
+    //adds p and child processes to the ns namespace
     if (ns->n_type == NS_PID){
         p->namespaces[NS_PID] = ns;
 
@@ -43,11 +45,15 @@ nsd_t *allocNamespace(int type){
     nsd_t *ns;
     
     if (type == NS_PID){
+        //check if there are free ns
         if (list_empty(&pid_nsFree_h))
             return NULL;
         
+        //gets first element, removes it from list of free ns
         ns = container_of(pid_nsFree_h.next, nsd_t, n_link);
         list_del(pid_nsFree_h.next);
+
+        //initializes ns, adds it to list of active namespaces
         ns->n_type = type;
         INIT_LIST_HEAD(&ns->n_link);
         list_add(&ns->n_link, &pid_nsList_h);
@@ -61,9 +67,11 @@ nsd_t *allocNamespace(int type){
 void freeNamespace(nsd_t *ns){
     struct list_head *cursor;
 
-    // pid list
+    //check every element in the active namespaces list
     list_for_each(cursor, &pid_nsList_h){
+        //if element is in namespace ns
         if (ns == container_of(cursor, nsd_t, n_link)){
+            //removes from active ns list, adds to free ns list
             list_del(&ns->n_link);
             list_add(&ns->n_link, &pid_nsFree_h);
         }
