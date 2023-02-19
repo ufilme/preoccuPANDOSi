105283554264ea773ddb8da8cb12b5b044091b89
BMMXIV <bernymm2002@gmail.com>
2023-02-19 19:44:46 +0100

    add: ash.c comments

--- pandos/phase1/src/ash.c
+++ pandos/phase1/src/ash.c
@@ -12,6 +12,7 @@ semd_t *getSemFromASH(int *semAdd){
     semd_t *s;
     struct hlist_node *cursor;
 
+    //search a semaphore with key semAdd in active semaphores list
     hlist_for_each(cursor, &semd_h){
         s = container_of(cursor, semd_t, s_link);
         if (s->s_key == semAdd){
@@ -19,12 +20,16 @@ semd_t *getSemFromASH(int *semAdd){
         }
     }
 
+    //return NULL if such semaphore doesn't exist
     return NULL;
 };
 
 void emptyBlockedProcQ(semd_t *s){
+    //if the semaphore list of blocked pcb is empty
     if (emptyProcQ(&s->s_procq)){
+        //removes sem from active sem list
         hlist_del(&s->s_link);
+        //adds the semaphore to the free sem list
         list_add(&s->s_freelink, &semdFree_h);
     }
 }
@@ -33,38 +38,51 @@ int insertBlocked(int *semAdd, pcb_t *p){
     semd_t *s = getSemFromASH(semAdd);
 
     if (s == NULL){
+        //allocating a new semd is not possible, return TRUE
         if (list_empty(&semdFree_h))
             return TRUE;
         
+        //gets an element (and removes it) from the free semaphores list
         s = container_of(semdFree_h.next, semd_t, s_freelink);
         list_del(semdFree_h.next);
 
+        //initializes the semaphore
         p->p_semAdd = semAdd;
         s->s_key = semAdd;
         INIT_LIST_HEAD(&s->s_procq);
+        //adds p to its queue
         insertProcQ(&s->s_procq, p);
+        //adds s to active semaphores list
         hlist_add_head(&s->s_link, &semd_h);
     } else {
+        //semaphore with key semAdd is in the active semaphores list
         p->p_semAdd = semAdd;
+        //adds p to the semaphore queue
         insertProcQ(&s->s_procq, p);
     }
 
+    //if there isn't a semaphore with key semAdd and there are no free semaphores
     return FALSE;
 };
 
 pcb_t *removeBlocked(int *semAdd){
     semd_t *s = getSemFromASH(semAdd);
 
+    //if there's no active sem with key semAdd
     if (s == NULL)
         return NULL;
     
+    //remove first pcb from blocked pcb list of the semaphore
     pcb_t *p = removeProcQ(&s->s_procq);
 
+    //if there are no blocked pcb
     if (p == NULL)
         return NULL;
     
+    //p is no longer blocked on the semaphore s
     p->p_semAdd = NULL;
 
+    //if list of blocked pcb is now empty removes sem from active list and adds it to free list
     emptyBlockedProcQ(s);
 
     return p;
@@ -73,16 +91,21 @@ pcb_t *removeBlocked(int *semAdd){
 pcb_t *outBlocked(pcb_t *p){
     semd_t *s = getSemFromASH(p->p_semAdd);
 
+    //error: no active semaphore with key p_semAdd
     if (s == NULL)
         return NULL;
 
+    //removes p from s blocked pcb list
     p = outProcQ(&s->s_procq, p);
 
+    //if p was not in the blocked pcb list of semaphore s
     if (p == NULL)
         return NULL;
 
+    //p is no longer blocked on the semaphore s
     p->p_semAdd = NULL;
     
+    //if list of blocked pcb is now empty removes sem from active list and adds it to free list
     emptyBlockedProcQ(s);
 
     return p;
@@ -91,17 +114,21 @@ pcb_t *outBlocked(pcb_t *p){
 pcb_t *headBlocked(int *semAdd){
     semd_t *s = getSemFromASH(semAdd);
 
+    //there's no active semaphore with key semAdd or it's blocked pcb list is empty
     if (s == NULL || emptyProcQ(&s->s_procq))
         return NULL;
 
+    //return first pcb from blocked pcb list without removing it
     pcb_t *p = headProcQ(&s->s_procq);
 
     return p;
 };
 
 void initASH(){
+    //semdFree_h = empty list
     INIT_LIST_HEAD(&semdFree_h);
 
+    //adds every semd_table element to the semdFree_h list
     for (int i = 0; i < MAXPROC; i++){
         semd_t *s = &semd_table[i];
         list_add(&s->s_freelink, &semdFree_h);
